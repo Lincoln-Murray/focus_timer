5423d979a7b8736fcf851da3d998e6ca911b80e6
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-05-04 08:35:48 +0800

    Update main.js
    Clean-up and consistent semicolon use.

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ time = 600;
 playing = false
 
 function start(){
-    end()
+    end();
     var endDate = new Date().getTime() + (time)*1000;
     playing = true
     var timer = setInterval(function() {
         if (playing == true){
             let now = new Date().getTime();
             let time_elepsed = endDate - now;
-            let time_elepsed_seconds = Math.floor(time_elepsed/1000)- Math.floor(time_elepsed/ (1000 * 60 * 60 * 24))
-            update_time(time_elepsed_seconds)
+            let time_elepsed_seconds = Math.floor(time_elepsed/1000)- Math.floor(time_elepsed/ (1000 * 60 * 60 * 24));
+            update_time(time_elepsed_seconds);
         }
         else{
             clearInterval(timer);
@@ -19,65 +19,66 @@ function start(){
 }
 
 function end(){
-    time = collect_input()
-    update_time(time)
-    playing = false
+    time = collect_input();
+    update_time(time);
+    playing = false;
 }
 
 function pause_unpause(){
     if (playing == true){
-        document.getElementById('anim_dot').style.animationName = 'shrink'
-        end()
+        document.getElementById('anim_dot').style.animationName = 'shrink';
+        end();
     }
     else{
-        document.getElementById('anim_dot').style.animationName = 'expand'
-        start()
+        document.getElementById('anim_dot').style.animationName = 'expand';
+        start();
     }
 }
 
 function collect_input(){
-    input = document.getElementById('timer').value
+    input = document.getElementById('timer').value;
     input_array = input.split(':');
-    let seconds = parseInt(input_array[input_array.length-1],10)
+    let seconds = parseInt(input_array[input_array.length-1],10);
     if (input_array.length >= 2){
-        seconds += parseInt(input_array[input_array.length-2],10) * 60
+        seconds += parseInt(input_array[input_array.length-2],10) * 60;
         if (input_array.length >= 3){
-            seconds += parseInt(input_array[input_array.length-3],10) * 3600
+            seconds += parseInt(input_array[input_array.length-3],10) * 3600;
         }
     }
-    return seconds
+    return seconds;
 }
 
 function update_time(_temp_time){
-    time_string = ''
-    temp_time = _temp_time
+    time_string = '';
+    temp_time = _temp_time;
     if (temp_time>= 60){
         if (temp_time>= 3600){
-            time_string = time_string + String(Math.floor(temp_time/3600)) + ':'
-            temp_time -= (Math.floor(temp_time/3600))*3600
+            time_string = time_string + String(Math.floor(temp_time/3600)) + ':';
+            temp_time -= (Math.floor(temp_time/3600))*3600;
             if (Math.floor(temp_time/60) < 10){
-                time_string = time_string + '0' + String(Math.floor(temp_time/60)) + ':'
+                time_string = time_string + '0' + String(Math.floor(temp_time/60)) + ':';
             }
             else{
-                time_string = time_string + String(Math.floor(temp_time/60)) + ':'
+                time_string = time_string + String(Math.floor(temp_time/60)) + ':';
             }
         }
         else{
-            time_string = time_string + String(Math.floor(temp_time/60)) + ':'
+            time_string = time_string + String(Math.floor(temp_time/60)) + ':';
         }
-        temp_time -= Math.floor(temp_time/60)*60
+        temp_time -= Math.floor(temp_time/60)*60;
         if (temp_time < 10){
-            time_string = time_string + '0' + String(temp_time)
+            time_string = time_string + '0' + String(temp_time);
         }
         else{
-            time_string = time_string + String(temp_time)
+            time_string = time_string + String(temp_time);
         }
     }
     else{
-        time_string = time_string + String(temp_time)
+        time_string = time_string + String(temp_time);
     }
-    document.getElementById('timer').value = time_string
+    document.getElementById('timer').value = time_string;
 }
+
 
 document.addEventListener("click", (evt) => {
     const timer = document.getElementById("timer");
@@ -86,22 +87,19 @@ document.addEventListener("click", (evt) => {
     
     let El = evt.target;
     if (El === dot){
-        pause_unpause()
+        pause_unpause();
     }
     else if (El === anim_dot){
-        pause_unpause()
+        pause_unpause();
     }
     else{
-        end()
+        end();
     }
-    //El = El.parentNode;
-
 });
 
 document.addEventListener("keydown", (evt) => {
-    console.log(evt.code)
     if(evt.code === 'Space') {
-        pause_unpause()
+        pause_unpause();
     }
 });
 
